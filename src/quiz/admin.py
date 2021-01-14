afa4a2ef7ded63737a78aa4c2df4edd7b0c4b295
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-14 22:15:48 +0300

    added models

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Quiz, Question, Answer
+
+admin.site.register(Category)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
 
 # Register your models here.
