ee19aaf7578515d1ba9eba7689606e8681d8b665
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-14 22:38:57 +0300

    added custom admin

--- src/quiz/admin.py
+++ src/quiz/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+import nested_admin
 from .models import Category, Quiz, Question, Answer
 
+
+class AnswerInline(nested_admin.NestedTabularInline):  # ınline olarak hazırlıyor
+    model = Answer
+
+
+class QuestionInline(nested_admin.NestedTabularInline):
+    model = Question
+    inlines = [AnswerInline]
+
+
+class QuizAdmin(nested_admin.NestedModelAdmin):
+    model = Quiz
+    inlines = [QuestionInline]
+
+
 admin.site.register(Category)
-admin.site.register(Quiz)
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
 
