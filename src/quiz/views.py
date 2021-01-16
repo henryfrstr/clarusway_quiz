8d2b4e6c60781c537a95727d4e0d2a414a174ec6
henryfrstr <63148122+henryfrstr@users.noreply.github.com>
2021-01-16 20:37:07 +0300

    added permissions

--- src/quiz/views.py
+++ src/quiz/views.py
@@ -4,15 +4,20 @@ from rest_framework import generics
 from .models import Category, Question, Quiz
 from .serializers import CategorySerializer, CategoryDetailSerializer, QuestionSerializer
 # from .pagination import MyPagination
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 
 class CategoryList(generics.ListAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication, SessionAuthentication]
 
 
 class CategoryDetail(generics.ListAPIView):
     serializer_class = CategoryDetailSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Quiz.objects.all()
@@ -23,6 +28,7 @@ class CategoryDetail(generics.ListAPIView):
 
 class QuizDetail(generics.ListAPIView):
     serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticated]
     # pagination_class = MyPagination
     # pagination_class = [Pa]
 
